baa081389e4ae4f685c35544aca577ec2991df2d
oscar01mx <oscar01mx@gmail.com>
2026-03-04 19:07:57 -0600

    fixed bug

--- src/main/java/com/example/hellotesting/HelloWorldController.java
+++ src/main/java/com/example/hellotesting/HelloWorldController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class HelloWorldController {
+public class HelloWorldController throws InterruptedException {
     
     @GetMapping("/hello")
     public String hello()  {
